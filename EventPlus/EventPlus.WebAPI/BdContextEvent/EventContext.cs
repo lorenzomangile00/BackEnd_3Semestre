727b29288776d9cf1268bc215fb15d07f24c195c
LORENZO MANGILE FARIA <53101823811@info.local>
2026-03-05 16:40:48 -0300

    Realizamos o Scaffold do EventPlus

---
+++ EventPlus/EventPlus.WebAPI/BdContextEvent/EventContext.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using EventPlus.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlus.WebAPI.BdContextEvent;
+
+public partial class EventContext : DbContext
+{
+    public EventContext()
+    {
+    }
+
+    public EventContext(DbContextOptions<EventContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ComentarioEvento> ComentarioEventos { get; set; }
+
+    public virtual DbSet<Evento> Eventos { get; set; }
+
+    public virtual DbSet<Instituicao> Instituicaos { get; set; }
+
+    public virtual DbSet<Presenca> Presencas { get; set; }
+
+    public virtual DbSet<TipoEvento> TipoEventos { get; set; }
+
+    public virtual DbSet<TipoUsuario> TipoUsuarios { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EventPlus;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ComentarioEvento>(entity =>
+        {
+            entity.HasKey(e => e.IdComentarioEvento).HasName("PK__Comentar__4305A1F1656BAD53");
+
+            entity.Property(e => e.IdComentarioEvento).HasDefaultValueSql("(newid())");
+
+            entity.HasOne(d => d.IdEventoNavigation).WithMany(p => p.ComentarioEventos).HasConstraintName("FK__Comentari__IdEve__76969D2E");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.ComentarioEventos).HasConstraintName("FK__Comentari__IdUsu__75A278F5");
+        });
+
+        modelBuilder.Entity<Evento>(entity =>
+        {
+            entity.HasKey(e => e.IdEvento).HasName("PK__Evento__034EFC04FE28E81A");
+
+            entity.Property(e => e.IdEvento).HasDefaultValueSql("(newid())");
+
+            entity.HasOne(d => d.IdInstituicaoNavigation).WithMany(p => p.Eventos).HasConstraintName("FK__Evento__IdInstit__6D0D32F4");
+
+            entity.HasOne(d => d.IdTipoEventoNavigation).WithMany(p => p.Eventos).HasConstraintName("FK__Evento__IdTipoEv__6C190EBB");
+        });
+
+        modelBuilder.Entity<Instituicao>(entity =>
+        {
+            entity.HasKey(e => e.IdInstituicao).HasName("PK__Institui__B771C0D85DCB7C95");
+
+            entity.Property(e => e.IdInstituicao).HasDefaultValueSql("(newid())");
+        });
+
+        modelBuilder.Entity<Presenca>(entity =>
+        {
+            entity.HasKey(e => e.IdPresenca).HasName("PK__Presenca__50FB6F5DC86BBB59");
+
+            entity.Property(e => e.IdPresenca).HasDefaultValueSql("(newid())");
+
+            entity.HasOne(d => d.IdEventoNavigation).WithMany(p => p.Presencas).HasConstraintName("FK__Presenca__IdEven__71D1E811");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Presencas).HasConstraintName("FK__Presenca__IdUsua__70DDC3D8");
+        });
+
+        modelBuilder.Entity<TipoEvento>(entity =>
+        {
+            entity.HasKey(e => e.IdTipoEvento).HasName("PK__TipoEven__CDB3A3BEA75C578B");
+
+            entity.Property(e => e.IdTipoEvento).HasDefaultValueSql("(newid())");
+        });
+
+        modelBuilder.Entity<TipoUsuario>(entity =>
+        {
+            entity.HasKey(e => e.IdTipoUsuario).HasName("PK__TipoUsua__CA04062B96448E4E");
+
+            entity.Property(e => e.IdTipoUsuario).HasDefaultValueSql("(newid())");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__5B65BF973E494F48");
+
+            entity.Property(e => e.IdUsuario).HasDefaultValueSql("(newid())");
+
+            entity.HasOne(d => d.IdTipoUsuarioNavigation).WithMany(p => p.Usuarios).HasConstraintName("FK__Usuario__IdTipoU__6477ECF3");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
