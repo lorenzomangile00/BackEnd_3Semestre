266ddeb74c9843e90689cbb939e7e346ddb9fe80
LORENZO MANGILE FARIA <53101823811@info.local>
2026-03-24 16:48:00 -0300

    Comecamos o Connect Plus

---
+++ ConnectPlus/ConnectPlus/Interfaces/IContatoRepository.cs
@@ -0,0 +1,12 @@
+﻿using ConnectPlus.Models;
+
+namespace ConnectPlus.Interfaces;
+
+public interface IContatoRepository
+{
+    void Cadastrar(Contato contato);
+    List<Contato> Listar();
+    Contato BuscarPorId(Guid id);
+    void Atualizar(Contato contato);
+    void Deletar(Guid id);
+}
---
+++ ConnectPlus/ConnectPlus/Models/Contato.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectPlus.Models;
+
+[Table("Contato")]
+public partial class Contato
+{
+    [Key]
+    public Guid IdContato { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? Imagem { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string FormaContato { get; set; } = null!;
+
+    public Guid? IdTipoContato { get; set; }
+
+    [ForeignKey("IdTipoContato")]
+    [InverseProperty("Contatos")]
+    public virtual TipoContato? IdTipoContatoNavigation { get; set; }
+}
