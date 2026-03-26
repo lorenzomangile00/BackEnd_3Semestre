617b753d3dce3c553417f2301410b4208a77415a
LORENZO MANGILE FARIA <53101823811@info.local>
2026-03-26 13:43:46 -0300

    Filmes

--- APIs/Filmes.WebAPI/obj/Debug/net10.0/Filmes.WebAPI.AssemblyInfo.cs
+++ APIs/Filmes.WebAPI/obj/Debug/net10.0/Filmes.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0578469cd4e8f78ccc88462472383d61132a2b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266ddeb74c9843e90689cbb939e7e346ddb9fe80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
